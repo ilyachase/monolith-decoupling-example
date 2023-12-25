03eede48d35a52d3a068ef1c4df601d4e1c9a878
Ilya Levin <ilya.chase@yandex.ru>
2023-12-25 13:23:48 +0500

    Clients cleanup

--- customer-service/src/Common/Client/RestaurantServiceClient.php
+++ customer-service/src/Common/Client/RestaurantServiceClient.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Common\Client;
 
-use App\Common\Dto\Order;
 use App\Common\Dto\Restaurant;
 use RuntimeException;
 
@@ -23,21 +22,6 @@ class RestaurantServiceClient extends AbstractHttpClient
         }
 
         return $this->serializer->deserialize($response->getContent(), Restaurant::class, 'json');
-    }
-
-    public function acceptOrder(Order $orderDto): bool
-    {
-        $response = $this->sendServiceRequest(
-            uri: '/service-restaurant/orders/actions/accept',
-            requestBody: $this->serializer->normalize($orderDto),
-            method: 'POST'
-        );
-
-        if (200 !== $response->getStatusCode()) {
-            throw new RuntimeException('Unexpected response code');
-        }
-
-        return $this->serializer->decode(data: $response->getContent(), format: 'json');
     }
 
     protected function getServiceName(): string
